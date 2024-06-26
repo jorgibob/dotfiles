2c6755a5855657cc4839171c4c5d8e1d365bd45c
Anders Ravenholt <aravenholt@gmail.com>
2024-06-26 09:49:22 -0400

    update setup script

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # brew
 brew install asdf
 brew install nvm
@@ -19,3 +21,16 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+
+# Setup configs
+echo ".dotfiles" >> .gitignore
+git clone --bare https://github.com/ravenholt/dotfiles.git $HOME/.dotfiles
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+mkdir -p .dotfiles-backup && \
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .dotfiles-backup/{}
+
+dotfiles checkout
+dotfiles config --local status.showUntrackedFiles no
+
