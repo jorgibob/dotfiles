eb0b331b0c1e6efa5f9ebebf122bc2f30234154e
jorgi <aravenholt@gmail.com>
2025-04-25 19:54:32 -0400

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # brew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew upgrade
+  
 brew install asdf
 brew install pyenv
 brew install nvm
