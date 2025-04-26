941e8194f6c9a6f7da2c0ad5e5c97f1733cbac83
jorgi <aravenholt@gmail.com>
2025-04-26 19:38:08 -0400

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -67,7 +67,7 @@ git clone --bare https://github.com/jorgibob/dotfiles.git $HOME/.dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 mkdir -p .dotfiles-backup && \
-config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
 xargs -I{} mv {} .dotfiles-backup/{}
 
 dotfiles checkout
